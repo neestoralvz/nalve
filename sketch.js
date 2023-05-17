802bcea59a840a7d3e1c114105fcf62540b99fcd
neestoralvz <133523729+neestoralvz@users.noreply.github.com>
2023-05-16 18:49:37 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,32 @@
 function setup() {
   createCanvas(400, 400);
-  background(135, 206, 235); // Color del cielo
 }
 
 function draw() {
-  translate(width / 2, height); // Centramos la pirámide en la página
+  background(255);
 
-  // Dibujamos la pirámide
-  fill(210, 105, 30); // Color marrón de la pirámide
-  triangle(-50, -100, 50, -100, 0, -200);
+  stroke(0);
+  fill(200);
+
+  // Dibuja la base de la pirámide
+  beginShape();
+  vertex(50, 300);
+  vertex(350, 300);
+  vertex(300, 200);
+  vertex(100, 200);
+  endShape(CLOSE);
+
+  // Dibuja la cara frontal de la pirámide
+  beginShape();
+  vertex(100, 200);
+  vertex(300, 200);
+  vertex(200, 100);
+  endShape(CLOSE);
+
+  // Dibuja la cara lateral de la pirámide
+  beginShape();
+  vertex(300, 200);
+  vertex(350, 300);
+  vertex(200, 100);
+  endShape(CLOSE);
 }
